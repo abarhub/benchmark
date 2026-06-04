a8abc63d3a5a0c81fe81667dfea7f1c2fbc2ab84
abarhub <abarhub@gmail.com>
2026-06-04 23:30:35 +0200

    multiplication matriciel

--- java/src/main/java/com/exemple/HelloWorld.java
+++ java/src/main/java/com/exemple/HelloWorld.java
@@ -2,8 +2,50 @@ package com.exemple;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    private static void helloworld(){
         System.out.println("Hello World !");
+    }
+
+    private static void multi(String[] args){
+
+        int lenx=3;
+
+        if(args!=null&&args.length>0){
+            var s=args[0];
+            var n=Integer.parseInt(s);
+            if(n>0){
+                lenx=n;
+            }
+        }
+        System.out.println("len="+lenx);
+
+        double[][] tab1=new double[lenx][lenx];
+        double[][] tab2=new double[lenx][lenx];
+        double[][] res=new double[lenx][lenx];
+
+        for(int i=0;i<lenx;i++){
+
+            for(int j=0;j<lenx;j++){
+
+                double m=0.0;
+                for(int k=0;k<lenx;k++){
+                    m+=tab1[i][k]*tab2[k][j];
+                }
+                res[i][j]=m;
+
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        if(false){
+            helloworld();
+        } else if(true) {
+            multi(args);
+        }
     }
 
 }
