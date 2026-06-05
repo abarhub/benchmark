76c516dc79dd75b31758a7aed3b99bffef73f12c
abarhub <abarhub@gmail.com>
2026-06-05 21:27:13 +0200

    implémentation multithread en java

--- java/src/main/java/com/exemple/HelloWorld.java
+++ java/src/main/java/com/exemple/HelloWorld.java
@@ -1,17 +1,25 @@
 package com.exemple;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class HelloWorld {
 
     private static void helloworld(){
         System.out.println("Hello World !");
     }
 
-    private static void multi(String[] args){
+    private static void multi(List<String> args){
 
         int lenx=3;
 
-        if(args!=null&&args.length>0){
-            var s=args[0];
+        if(args!=null&&args.size()>0){
+            var s=args.getFirst();
             var n=Integer.parseInt(s);
             if(n>0){
                 lenx=n;
@@ -38,13 +46,96 @@ public class HelloWorld {
         }
 
     }
+    
+    private static void multiThread(List<String> args){
+        int lenx=3;
+
+        if(args!=null&&args.size()>0){
+            var s=args.getFirst();
+            var n=Integer.parseInt(s);
+            if(n>0){
+                lenx=n;
+            }
+        }
+        System.out.println("len="+lenx);
+
+        //ExecutorService executor = Executors.newFixedThreadPool(10);
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
+        double[][] tab1=new double[lenx][lenx];
+        double[][] tab2=new double[lenx][lenx];
+        double[][] res=new double[lenx][lenx];
+
+        final int len0=lenx;
+        List<Future<Double>> listeResultat=new ArrayList<>();
+
+        for(int i=0;i<lenx;i++){
+
+            for(int j=0;j<lenx;j++){
+
+                final int i0=i;
+                final int j0=j;
+                
+                Callable<Double> callableTask = () -> {
+                    double m=0.0;
+                    for(int k=0;k<len0;k++){
+                        m+=tab1[i0][k]*tab2[k][j0];
+                    }
+                    res[i0][j0]=m;
+                    return m;
+                };
+
+                var res2=executor.submit(callableTask);
+                listeResultat.add(res2);
+            }
+
+        }
+
+        for(var future:listeResultat){
+            Double result = null;
+            try {
+                result = future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
 
     public static void main(String[] args) {
         
-        if(false){
+        int operation=3;
+        List<String> liste=new ArrayList<>();
+        if(args!=null){
+            for(var s:args){
+                liste.add(s);
+            }
+        }
+
+        if(liste.size()>0){
+            switch(args[0]){
+                case "helloword":
+                    operation=1;
+                    liste.removeFirst();
+                    break;
+                case "multi":
+                    operation=2;
+                    liste.removeFirst();
+                    break;
+                case "multithread":
+                    operation=3;
+                    liste.removeFirst();
+                    break;
+            }
+        }
+
+        if(operation==1){
             helloworld();
-        } else if(true) {
-            multi(args);
+        } else if(operation==2) {
+            multi(liste);
+        } else if(operation==3) {
+            multiThread(liste);
         }
     }
 
