65a052a6df7f31857bfde2e53dfaa5696f0f2eaf
abarhub <abarhub@gmail.com>
2026-06-06 11:48:14 +0200

    gestion des arguments pour rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,18 +1,25 @@
+use std::env;
+
 fn hello() {
     println!("Hello, world!");
 }
 
-fn mult() {
-    const LEN: usize = 3;
+fn mult(args_cp: Vec<String>) {
+    let mut len: usize = 3;
 
-    let mut tab1: [[f64; LEN]; LEN] = [[0.0; LEN]; LEN];
-    let mut tab2: [[f64; LEN]; LEN] = [[0.0; LEN]; LEN];
-    let mut res: [[f64; LEN]; LEN] = [[0.0; LEN]; LEN];
+    if args_cp.len() > 0 {
+        let s = &args_cp[0];
+        len = s.parse::<usize>().unwrap();
+    }
+
+    let mut tab1 = vec![vec![0.0; len]; len];
+    let mut tab2 = vec![vec![0.0; len]; len];
+    let mut res = vec![vec![0.0; len]; len];
 
     let mut pos = 0;
 
-    for i in 0..LEN - 1 {
-        for j in 0..LEN - 1 {
+    for i in 0..len - 1 {
+        for j in 0..len - 1 {
             pos += i + j;
             if pos % 2 == 0 {
                 tab1[i][j] = 1.1;
@@ -27,11 +34,11 @@ fn mult() {
         }
     }
 
-    for i in 0..LEN - 1 {
-        for j in 0..LEN - 1 {
+    for i in 0..len - 1 {
+        for j in 0..len - 1 {
             let mut m = 0.0;
 
-            for n in 0..LEN - 1 {
+            for n in 0..len - 1 {
                 m += tab1[i][n] * tab2[n][j];
             }
 
@@ -41,9 +48,27 @@ fn mult() {
 }
 
 fn main() {
-    if false {
+    let args: Vec<String> = env::args().collect();
+    let mut args_cp: Vec<String> = args.clone();
+
+    args_cp.remove(0);
+
+    let mut operateur = 2;
+
+    if args_cp.len() > 0 {
+        let op = &args_cp[0];
+        if op == "helloword" {
+            operateur = 1;
+            args_cp.remove(0);
+        } else if op == "mult" {
+            operateur = 2;
+            args_cp.remove(0);
+        }
+    }
+
+    if operateur == 1 {
         hello();
-    } else if true {
-        mult();
+    } else if operateur == 2 {
+        mult(args_cp);
     }
 }
